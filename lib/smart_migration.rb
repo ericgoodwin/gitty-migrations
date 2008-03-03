886ef0e2b49baf880192236191ad550676892e4d
Eric Goodwin <eric@ericgoodwin.com>
2008-03-03 12:10:40 -0800

    Only run the stash commands if we have changes that haven't been commited yet

--- lib/smart_migration.rb
+++ lib/smart_migration.rb
@@ -18,6 +18,7 @@ module SmartMigration
       g = Git.open("#{RAILS_ROOT}")
       raise "Couldn't find the .git directory. Does this project use git?" if g.nil?
       @@smart_migration[:run] = true
+      @@smart_migration[:stash] = (g.status.changed.size > 0) ? true : false
       @@smart_migration[:git] = g
       @@smart_migration[:branch] = "smart_migration_".concat Digest::MD5.hexdigest("#{Time.now}-#{revision_name}")
       @@smart_migration[:revision] = revision_name
@@ -35,7 +36,7 @@ module SmartMigration
       if m[:run]
         say "Migrating with git revision #{m[:revision]}"
         g = m[:git]
-        g.branch(m[:original_branch]).stashes.save(m[:revision])
+        g.branch(m[:original_branch]).stashes.save(m[:revision]) if @@smart_migration[:stash]
         g.branch(m[:branch]).checkout
         commit = g.gcommit("#{m[:revision]}")
 
@@ -61,7 +62,7 @@ module SmartMigration
         say "Done migration. Switching back to your normal branch"
         g = @@smart_migration[:git]
         g.branch(m[:original_branch]).checkout
-        g.branch(m[:original_branch]).stashes.apply
+        g.branch(m[:original_branch]).stashes.apply if @@smart_migration[:stash]
         g.branch(m[:branch]).delete
         # If we've already deleted the branch, then don't revert again
         # revert_to_original will be called in the after migrate even if we couldn't find the commit
